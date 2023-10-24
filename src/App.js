db8fbc62368aa9ef8ed4cec5780d549b79450933
mayw-acast <may.woollcott@acast.com>
2023-10-24 10:02:22 -0400

    home page initial parallax

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(window.pageYOffset);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <div className='homePage'>
+        <div className='introContainer'>
+          <p className='introText'>hi, i'm may!</p>
+          <p className='labelText'>SOFTWARE ENGINEER</p>
+        </div>
+        <div
+          className='specialtiesContainer'
+          style={{ transform: `translateX(-${offsetY * 2}px)` }}
         >
-          Learn React
-        </a>
-      </header>
+          <p className='specialtiesHeaderText'>SPECIALTIES:</p>
+          <div className='specialtiesTextContainer'>
+            <p className='specialtiesText'>FRONT-END</p>
+            <p className='bulletText'>•</p>
+          </div>
+          <div className='specialtiesTextContainer'>
+            <p className='specialtiesText'>BACK-END</p>
+            <p className='bulletText'>•</p>
+          </div>
+          <div className='specialtiesTextContainer'>
+            <p className='specialtiesText'>MOBILE</p>
+            <p className='bulletText'>•</p>
+          </div>
+          <div className='specialtiesTextContainer'>
+            <p className='specialtiesText'>UX/UI</p>
+            <p className='bulletText'>•</p>
+          </div>
+        </div>
+        <div
+          className='graySquare'
+          style={{ transform: `translateX(${offsetY * 2}px)` }}
+        ></div>
+        <div
+          className='pinkCircle'
+          style={{
+            transform: `translateY(${offsetY * 1.2}px) translateX(-${
+              offsetY * 0.9
+            }px)`,
+            top: window.innerHeight / 2 - 110,
+            left: window.innerWidth / 2 - 110,
+          }}
+        ></div>
+      </div>
+      <div className='projectsPage'>
+        <p className='introText'>projects</p>
+      </div>
     </div>
   );
 }
