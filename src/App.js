7bccb52ad7c1c9132c983a9057871556e412918c
mayw-acast <may.woollcott@acast.com>
2023-10-25 21:47:49 -0400

    project details added, most gifs

--- src/App.js
+++ src/App.js
@@ -23,19 +23,34 @@ function App() {
   const [librisLogClasses, setLibrisLogClasses] = useState('projectButton');
   const [bountifullClasses, setBountifullClasses] = useState('projectButton');
 
+  const [activeDescription, setActiveDescription] = useState();
+  const [activeStack, setActiveStack] = useState([]);
+  const [activeWebsite, setActiveWebsite] = useState();
+  const [activeGif, setActiveGif] = useState();
+
   const handleScroll = () => {
     setOffsetY(window.pageYOffset);
   };
 
   const onProjectButtonClick = (name) => {
     console.log('in view!');
-    setDiviiClasses('projectButton scale-out-center');
-    setPodplayClasses('projectButton scale-out-center');
-    setJujuClasses('projectButton scale-out-center');
-    setLexiClasses('projectButton scale-out-center');
-    setLibrisLogClasses('projectButton scale-out-center');
-    setBountifullClasses('projectButton scale-out-center');
-    setTimeout(() => {
+    if (!activeProject) {
+      setDiviiClasses('projectButton scale-out-center');
+      setPodplayClasses('projectButton scale-out-center');
+      setJujuClasses('projectButton scale-out-center');
+      setLexiClasses('projectButton scale-out-center');
+      setLibrisLogClasses('projectButton scale-out-center');
+      setBountifullClasses('projectButton scale-out-center');
+      setTimeout(() => {
+        setActiveProject(name);
+        setDiviiClasses('projectButtonActive');
+        setPodplayClasses('projectButtonActive');
+        setJujuClasses('projectButtonActive');
+        setLexiClasses('projectButtonActive');
+        setLibrisLogClasses('projectButtonActive');
+        setBountifullClasses('projectButtonActive');
+      }, 400);
+    } else {
       setActiveProject(name);
       setDiviiClasses('projectButtonActive');
       setPodplayClasses('projectButtonActive');
@@ -43,7 +58,7 @@ function App() {
       setLexiClasses('projectButtonActive');
       setLibrisLogClasses('projectButtonActive');
       setBountifullClasses('projectButtonActive');
-    }, 400);
+    }
     console.log(name);
   };
 
@@ -64,6 +79,64 @@ function App() {
       // });
     }
   }, [inView]);
+
+  useEffect(() => {
+    if (activeProject === 'divii') {
+      setActiveDescription(
+        'A fullstack tarot card app built for IOS and Android'
+      );
+      setActiveStack([
+        'Typescript',
+        'React Native',
+        'CSS',
+        'Mongo DB',
+        'Node.js',
+        'AWS',
+      ]);
+      setActiveWebsite('www.diviitarot.com');
+      setActiveGif('./diviiGif.gif');
+    }
+    if (activeProject === 'podplay') {
+      setActiveDescription(
+        'A web-based podcast interface with visual, unskippable ads, seeking, and custom skins'
+      );
+      setActiveStack(['React', 'CSS', 'NETLIFY']);
+      setActiveWebsite('github.com/podplayer');
+    }
+
+    if (activeProject === 'juju') {
+      setActiveDescription(
+        'A fullstack mobile app designed to send anonymous compliments and positivity. initial compliment sent via SMS from server with app invite'
+      );
+      setActiveStack([
+        'Typescript',
+        'React Native',
+        'CSS',
+        'Mongo DB',
+        'Twilio',
+      ]);
+      setActiveWebsite('github.com/juju');
+      setActiveGif('./jujuGif.gif');
+    }
+
+    if (activeProject === 'librisLog') {
+      setActiveDescription(
+        'A virtual library where you can track, log, and anylyze your reading habits'
+      );
+      setActiveStack(['React', 'CSS', 'Mongo DB', 'Node.js']);
+      setActiveWebsite('github.com/librislog');
+      setActiveGif('./librisGif.gif');
+    }
+
+    if (activeProject === 'lexi') {
+      setActiveDescription(
+        'The vocabulary app for grownups: Build your lexiCon'
+      );
+      setActiveStack(['Typescript', 'React Native', 'CSS', 'Mongo DB']);
+      setActiveWebsite('github.com/lexi');
+      setActiveGif('./lexiGif.gif');
+    }
+  }, [activeProject]);
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
@@ -174,30 +247,66 @@ function App() {
         )}
         {activeProject !== null && (
           <div className='activeProjectContainer'>
-            <div style={{ display: 'flex', flexDirection: 'column' }}>
-              <p>PROJECTS</p>
-              <div style={{ display: 'flex', flexDirection: 'row' }}>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                marginLeft: '30px',
+                alignItems: 'flex-start',
+              }}
+            >
+              <p className='activeProjectsHeader'>PROJECTS</p>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                }}
+              >
                 <div className='verticalButtonContainer'>
                   <div
                     className={diviiClasses}
                     style={{ backgroundColor: colorCodes.pink }}
                     onClick={() => onProjectButtonClick('divii')}
                   >
-                    <p className='projectTitleActive'>divii</p>
+                    <p
+                      className={
+                        activeProject === 'divii'
+                          ? 'projectTitleActive active'
+                          : 'projectTitleActive'
+                      }
+                    >
+                      divii
+                    </p>
                   </div>
                   <div
                     className={jujuClasses}
                     style={{ backgroundColor: colorCodes.lightGray }}
                     onClick={() => onProjectButtonClick('juju')}
                   >
-                    <p className='projectTitleActive'>juju</p>
+                    <p
+                      className={
+                        activeProject === 'juju'
+                          ? 'projectTitleActive active'
+                          : 'projectTitleActive'
+                      }
+                    >
+                      juju
+                    </p>
                   </div>
                   <div
                     className={librisLogClasses}
                     style={{ backgroundColor: colorCodes.mediumGray }}
                     onClick={() => onProjectButtonClick('librisLog')}
                   >
-                    <p className='projectTitleActive'>libris log</p>
+                    <p
+                      className={
+                        activeProject === 'librisLog'
+                          ? 'projectTitleActive active'
+                          : 'projectTitleActive'
+                      }
+                    >
+                      libris log
+                    </p>
                   </div>
                 </div>
                 <div className='verticalButtonContainer'>
@@ -206,24 +315,86 @@ function App() {
                     style={{ backgroundColor: colorCodes.mediumGray }}
                     onClick={() => onProjectButtonClick('podplay')}
                   >
-                    <p className='projectTitleActive'>podplay</p>
+                    <p
+                      className={
+                        activeProject === 'podplay'
+                          ? 'projectTitleActive active'
+                          : 'projectTitleActive'
+                      }
+                    >
+                      podplay
+                    </p>
                   </div>
                   <div
                     className={lexiClasses}
                     style={{ backgroundColor: colorCodes.pink }}
                     onClick={() => onProjectButtonClick('lexi')}
                   >
-                    <p className='projectTitleActive'>lexi</p>
+                    <p
+                      className={
+                        activeProject === 'lexi'
+                          ? 'projectTitleActive active'
+                          : 'projectTitleActive'
+                      }
+                    >
+                      lexi
+                    </p>
                   </div>
                   <div
                     className={bountifullClasses}
                     style={{ backgroundColor: colorCodes.lightGray }}
                     onClick={() => onProjectButtonClick('bountifull')}
                   >
-                    <p className='projectTitleActive'>bountifull</p>
+                    <p
+                      className={
+                        activeProject === 'bountifull'
+                          ? 'projectTitleActive active'
+                          : 'projectTitleActive'
+                      }
+                    >
+                      bountifull
+                    </p>
                   </div>
                 </div>
               </div>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'flex-start',
+                flexDirection: 'column',
+                width: '20%',
+              }}
+            >
+              <div className='descriptionHeaderContainer'>
+                <p className='descriptionName'>{activeProject}</p>
+                <a
+                  className='descriptionWebsite'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href={activeWebsite}
+                >
+                  {activeWebsite}
+                </a>
+              </div>
+              <p className='descriptionText'>{activeDescription}</p>
+              <div className='stackContainer'>
+                <p className='stackText'>STACK</p>
+                {activeStack.map((stack) => {
+                  return <p className='stackListText'>{stack}</p>;
+                })}
+              </div>
+            </div>
+            <div className='videoContainer'>
+              <img
+                className={
+                  activeProject === 'librisLog' || activeProject === 'podplay'
+                    ? 'computerGif easeload'
+                    : 'phoneGif easeload'
+                }
+                src={activeGif}
+              ></img>
             </div>
           </div>
         )}
